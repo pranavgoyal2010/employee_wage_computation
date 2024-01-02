20f3b8982a60856413d2b83d22b2ee7204683f7c
Pranav Goyal <pr945005@dal.ca>
2024-01-03 00:07:05 +0530

    Added UC1 to check Employee's attendance

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Welcome to Employee\n" +
-                "Wage Computation\n" +
-                "Program");
+
+        int status = (int) ((Math.random()*10)%2);
+
+        if(status==1)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
     }
 }
