95df68ea2a3210834f578f6cb56662589abfc681
Pranav Goyal <pr945005@dal.ca>
2024-01-03 11:12:29 +0530

    Modified the code to include UC1 in UC2

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,29 @@ public class Main {
          *  This is the UC 1 where we check if an employee is present or absent.
          *
          */
-        /*int status = (int) ((Math.random()*10)%2);
+        int status = (int) ((Math.random()*10)%2);
 
         if(status==1)
             System.out.println("Employee is Present");
         else
-            System.out.println("Employee is Absent");*/
+            System.out.println("Employee is Absent");
 
 
         /**
          * This is UC 2 where we compute the total daily wage of the employee.
          * */
+
         int hourlyWage = 20;
-        int numOfHours = 8;
-        int totalWage = hourlyWage*numOfHours;
-        System.out.println("Total Daily Wage: " + totalWage);
+        if(status==1) {
+            int numOfHours = 8;
+            int totalWage = hourlyWage * numOfHours;
+            System.out.println("Total Daily Wage: " + totalWage);
+        }
+        else {
+            int totalWage = 0;
+            System.out.println("Total Daily Wage: " + totalWage);
+        }
+
     }
 }
+
