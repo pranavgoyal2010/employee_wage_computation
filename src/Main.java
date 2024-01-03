2bee17273b9d892aac2bf7d6b1bca0d5313dbc44
Pranav Goyal <pr945005@dal.ca>
2024-01-03 13:28:14 +0530

    Added UC6 code to compute monthly wage for 100 hours or 20 days whichever is completed first.

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,50 @@ public class Main {
     public static void main(String[] args){
 
         /**
-         * This is UC 5 where total monthly wage is calculated.
+         * This is UC6 where total monthly wage is calculated based on whether 100 hours
+         * or 20 days are completed first.
          *
-         * It includes UC 1, UC 2, UC 3 and UC 4
+         * 
          * */
 
-
-        int status = (int) ((Math.random()*10)%3);
-
-        System.out.println("Employee Status: "+status);
-
+        int totalDays = 0;
+        int totalHours = 0;
         int hourlyWage = 20;
-        int days = 20;
 
-        switch (status) {
-            case 0:
-                System.out.println("Employee is Absent");
-                System.out.println("Total daily wage is: 0");
-                break;
-            case 1:
-                System.out.println("Employee is Present");
-                int hours = 4;
-                System.out.println("Part time total monthly wage is: " + hourlyWage * hours * days);
-                break;
-            case 2:
-                System.out.println("Employee is Present");
-                hours = 8;
-                System.out.println("Full time total monthly wage is: " + hourlyWage * hours * days);
-                break;
+
+        while(totalHours<100 && totalDays<20)
+        {
+            int status = (int) ((Math.random()*10)%3);
+
+            switch (status) {
+                case 0:
+                    break;
+                case 1:
+                    int hours = 4;
+                    totalHours += hours;
+                    break;
+                case 2:
+                    hours = 8;
+                    totalHours += hours;
+                    break;
+            }
+
+            totalDays++;
         }
+
+        System.out.println("Total Days: " + totalDays);
+
+        if(totalHours<100)
+        {
+            System.out.println("Total Hours: " + totalHours);
+            System.out.println("Employee's wages for this month: " + hourlyWage * totalHours);
+        }
+        else
+        {
+            System.out.println("Total Hours: 100");
+            System.out.println("Employee's wages for this month: " + hourlyWage * 100);
+        }
+
 
     }
 }
