4e2571bde65cdacbf8328d3c6981df7733b53cf8
Pranav Goyal <pr945005@dal.ca>
2024-01-04 11:05:30 +0530

    Corrected code for UC5

--- src/Main.java
+++ src/Main.java
@@ -7,31 +7,35 @@ public class Main {
          * It includes UC 1, UC 2, UC 3 and UC 4
          * */
 
-
-        int status = (int) ((Math.random()*10)%3);
-
-        System.out.println("Employee Status: "+status);
-
+        
+        int totalWage = 0;
         int hourlyWage = 20;
         int days = 20;
+        int hours = 0;
+        for(int i=1; i<=days; i++)
+        {
+            int status = (int) ((Math.random()*10)%3);
+            switch (status) {
+                case 0:
+                    System.out.println("Employee is Absent");
+                    System.out.println("Total daily wage is: 0");
+                    break;
+                case 1:
+                    System.out.println("Employee is Present");
+                    hours = 4;
+                    System.out.println("Part time total daily wage is: " + hourlyWage * hours);
+                    break;
+                case 2:
+                    System.out.println("Employee is Present");
+                    hours = 8;
+                    System.out.println("Full time total daily wage is: " + hourlyWage * hours);
+                    break;
+            }
 
-        switch (status) {
-            case 0:
-                System.out.println("Employee is Absent");
-                System.out.println("Total daily wage is: 0");
-                break;
-            case 1:
-                System.out.println("Employee is Present");
-                int hours = 4;
-                System.out.println("Part time total monthly wage is: " + hourlyWage * hours * days);
-                break;
-            case 2:
-                System.out.println("Employee is Present");
-                hours = 8;
-                System.out.println("Full time total monthly wage is: " + hourlyWage * hours * days);
-                break;
+            totalWage += hourlyWage*hours;
         }
 
+        System.out.println("Employee's total monthly wage: "+totalWage);
     }
 }
 
